7f777a8b1d5d9721cba4a1f545feb55f087c8bf9
hariramprasanth balasubramanian <hariramprasanth.b@thoughtworks.com>
2022-04-13 10:27:30 +0530

    #785 | Refactor. change extension of log files | [Hari/mathu]

--- .ignore/init.sh
+++ .ignore/init.sh
@@ -6,14 +6,14 @@ timestamp(){
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
-nohup docker pull mysql &> dockerpull.txt &
-nohup docker pull flyway/flyway &> dockerpull.txt &
+nohup docker pull mysql &> dockerpull.log &
+nohup docker pull flyway/flyway &> dockerpull.log &
 
-touch initializationlog.txt
+touch initializationlog.log
 touch host-url.txt
 echo "export LOCAL_HOST_URL=http://localhost:8080" >> host-url.txt
 
-echo "$(timestamp) Locating required resources..." >> initializationlog.txt
+echo "$(timestamp) Locating required resources..." >> initializationlog.log
 
 IDTOKEN=null
 GATEWAY_ENDPOINT=null
@@ -24,16 +24,16 @@ HEADER2='Content-Type: application/x-amz-json-1.1'
 
 if [ $STAGE = "prod" ]
 then
-    echo "$(timestamp) Using Prod Artifact..." >> initializationlog.txt
+    echo "$(timestamp) Using Prod Artifact..." >> initializationlog.log
     GATEWAY_ENDPOINT='https://slygfw4sw7.execute-api.ap-south-1.amazonaws.com/Prod/get-signed-url'
     CLIENT_ID=7r012t0noqgjoaarjuc1u21v85
 elif [ $STAGE = "test" ]
 then
-    echo "$(timestamp) Using Test Artifact..." >> initializationlog.txt
+    echo "$(timestamp) Using Test Artifact..." >> initializationlog.log
     GATEWAY_ENDPOINT='https://ivcgd3sjsk.execute-api.ap-south-1.amazonaws.com/Prod/get-signed-url'
     CLIENT_ID=5jrqkesh41t56ragneqln5ilkl
 else
-    echo "$(timestamp) Using Dev Artifact..." >> initializationlog.txt
+    echo "$(timestamp) Using Dev Artifact..." >> initializationlog.log
     GATEWAY_ENDPOINT='https://piqcbc19ya.execute-api.ap-south-1.amazonaws.com/Prod/get-signed-url'
     CLIENT_ID=233o8b28j9k1423sh0vgujvn3c
 fi
@@ -58,7 +58,6 @@ done
 
 echo -e "\e[1;32m------------User Authenticated...--------------------\e[0m"
 echo ""
-echo "$(timestamp) starting of initilzation" >> timecheck.txt
 IDTOKEN=$(cat response.txt | jq -r .AuthenticationResult.IdToken)
 
 HTTP_RESPONSE=$(curl -s -o signedurl.txt -w "%{http_code}" -H "Authorization: $IDTOKEN" $GATEWAY_ENDPOINT)
@@ -74,20 +73,20 @@ ARTIFACT_URL=$(cat signedurl.txt)
 rm response.txt
 rm signedurl.txt
 
-echo "Artifact url obtained....." >> initializationlog.txt 
+echo "Artifact url obtained....." >> initializationlog.log 
 echo -e "\e[1;34m $(timestamp) --------- Downloading the artifacts ... --------------\e[0m"
 curl  --output guru-shifu.tar.gz "$ARTIFACT_URL"
-echo "$(timestamp) Artifact download complete." >> initializationlog.txt
+echo "$(timestamp) Artifact download complete." >> initializationlog.log
 echo ""
 echo "Setting up guru shifu. This may take around 2 minutes...."
 echo ""
-echo "$(timestamp) Unzipping guru-shifu tarball..." >> initializationlog.txt
+echo "$(timestamp) Unzipping guru-shifu tarball..." >> initializationlog.log
 tar -xf guru-shifu.tar.gz
-echo "$(timestamp) Unzip complete" >> initializationlog.txt
+echo "$(timestamp) Unzip complete" >> initializationlog.log
 
-echo "Docker build for fly way migrate"  >> initializationlog.txt
-docker build -t guru-shifu-db-migrations -f Dockerfile-flyway . >> initializationlog.txt
-echo "docker flyway build done..." >> initializationlog.txt
+echo "Docker build for fly way migrate"  >> initializationlog.log
+docker build -t guru-shifu-db-migrations -f Dockerfile-flyway . >> initializationlog.log
+echo "docker flyway build done..." >> initializationlog.log
 
 rm -R migration/
 rm Dockerfile-flyway 
--- .ignore/start.sh
+++ .ignore/start.sh
@@ -12,39 +12,38 @@ export HOME=/workspace/guru-shifu-gitpod/
 
 touch .env
 echo "REACT_APP_HOST_URL=https://8080-${GITPOD_WORKSPACE_URL#*//}" > .env
-echo "$(timestamp) Running docker-compose up in detach mode.." >> initializationlog.txt
+echo "$(timestamp) Running docker-compose up in detach mode.." >> initializationlog.log
 docker-compose -f docker-compose-gitpod.yml up -d --quiet-pull
 echo -e "\e[1;34m $(timestamp) Waiting for guru-shifu to start up.... \e[0m"
 STARTED=1
 until [ $STARTED == 0 ];
 do
-docker ps -a --filter "exited=0" | grep "guru-shifu-db-migrations" >> sqllogs.txt
+docker ps -a --filter "exited=0" | grep "guru-shifu-db-migrations" >> sqllogs.log
 STARTED=$?
 sleep 1
 done
 
-echo "$(timestamp) Docker compose completed." >> initializationlog.txt
+echo "$(timestamp) Docker compose completed." >> initializationlog.log
 
-echo "Replace backend url env variable" >> initializationlog.txt
+echo "Replace backend url env variable" >> initializationlog.log
 source .env
 source host-url.txt
 sed -i "s|$LOCAL_HOST_URL|$REACT_APP_HOST_URL|g" ./build/static/js/main.*.js
 echo "export LOCAL_HOST_URL=$REACT_APP_HOST_URL" > host-url.txt
 
-echo "Starting GuruShifu backend..." >> initializationlog.txt
+echo "Starting GuruShifu backend..." >> initializationlog.log
 
-nohup java -jar guru-shifu-boot-0.0.1-SNAPSHOT.jar &> springlog.txt &
+nohup java -jar guru-shifu-boot-0.0.1-SNAPSHOT.jar &> springlog.log &
 
 if [ $? == 0 ]
 then
-  echo "$(timestamp) Waiting for guru-shifu to start up.... " >> initializationlog.txt
+  echo "$(timestamp) Waiting for guru-shifu to start up.... " >> initializationlog.log
   until $(curl --output /dev/null --silent --head --fail http://localhost:8080/rectangle/feedback-history/); do
-    printf "."  >> initializationlog.txt
+    printf "."  >> initializationlog.log
     sleep 1
   done
-  echo "$(timestamp) Guru-shifu Backend started..."  >> initializationlog.txt
+  echo "$(timestamp) Guru-shifu Backend started..."  >> initializationlog.log
 fi
-echo "$(timestamp) GuruShifu frontend starting"  >> initializationlog.txt
-nohup serve -s build &> uilog.txt &
-echo "$(timestamp) ending of initilzation" >> timecheck.txt
+echo "$(timestamp) GuruShifu frontend starting"  >> initializationlog.log
+nohup serve -s build &> ui.log &
 cd /workspace
